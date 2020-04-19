e0e1a669033196a76c2949789a091dcd2b06aaa4
maciejmatu <maciek.matuszewski@gmail.com>
2020-04-19 16:57:55 +0200

    Show current player on the scoreboard

--- src/components/GameBoard/Scoreboard.tsx
+++ src/components/GameBoard/Scoreboard.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import map from "lodash/map";
 import { useBoardContext } from "./BoardContext";
+import classNames from "classnames";
 
 export const Scoreboard = () => {
-  const { State, playerID, playersInfo } = useBoardContext();
+  const { State, playerID, playersInfo, ctx } = useBoardContext();
 
   return (
     <div className="Scoreboard">
       {map(State.scoreboard, (userScore, userId) => {
-        const playerInfo = playersInfo.find(p => String(p.id) === userId)!;
+        const playerInfo = playersInfo.find((p) => String(p.id) === userId)!;
+        const isCurrentPlayer = ctx.currentPlayer === userId;
 
         return (
           <div key={userId} className="Scoreboard__row">
-            <span style={{ marginRight: 20 }}>
+            <span
+              style={{ marginRight: 20 }}
+              className={classNames(
+                "Scoreboard__player",
+                isCurrentPlayer && "Scoreboard__player--active"
+              )}
+            >
               {playerInfo.name}
               {playerID === userId && " (You)"}:
             </span>
