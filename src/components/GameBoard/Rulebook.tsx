b12ab9b93fe0fab84a8805814a547e6966dc6a30
maciejmatu <maciek.matuszewski@gmail.com>
2021-10-21 03:48:14 +0200

    Add english rules

--- src/components/GameBoard/Rulebook.tsx
+++ src/components/GameBoard/Rulebook.tsx
@@ -1,12 +1,22 @@
-import { Button } from "components/Button";
 import React from "react";
-import { Trans } from "react-i18next";
+import { Button } from "components/Button";
+import { Trans, useTranslation } from "react-i18next";
 import rules1 from "../../assets/rules/1.png";
 import rules2 from "../../assets/rules/2.png";
 import rules3 from "../../assets/rules/3.png";
+import rules4 from "../../assets/rules/1_en.png";
+import rules5 from "../../assets/rules/2_en.png";
+import rules6 from "../../assets/rules/3_en.png";
 import styles from "./Rulebook.module.scss";
 
+const rules = {
+  pl: [rules1, rules2, rules3],
+  en: [rules4, rules5, rules6],
+}
+
 export const Rulebook: React.FC<{ onClose(): any }> = ({ onClose }) => {
+  const { i18n } = useTranslation();
+
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
@@ -20,9 +30,9 @@ export const Rulebook: React.FC<{ onClose(): any }> = ({ onClose }) => {
         </Button>
 
         <div className={styles.modalScroll}>
-          <img className={styles.ruleImg} src={rules1} alt="" />
-          <img className={styles.ruleImg} src={rules2} alt="" />
-          <img className={styles.ruleImg} src={rules3} alt="" />
+          {rules[i18n.language.split("-")[0]]?.map((img => (
+            <img className={styles.ruleImg} src={img} alt="" />
+          )))}
         </div>
       </div>
     </div>
